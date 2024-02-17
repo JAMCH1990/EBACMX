383b14bd778e443ee38a95a0fcbe330e8468fd32
JAMCH1990 <mtro.jorge.martinez.chavez@gmail.com>
2024-02-17 17:47:18 -0600

    Factor 200

---
+++ Calculo 1.py
@@ -0,0 +1,13 @@
+# Prueba de Python
+# Se añadio la variable "c" 
+a = 10
+b = 30
+c = 30 * 10
+
+print ('resultado=', c)
+
+## Cambiado el factor 102 según requerimiento del negocio 
+## Cambiando el factor 110 según Junta Control  
+
+d = c * 110
+print ('resultado total=', d)
